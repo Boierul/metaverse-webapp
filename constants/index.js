c52328fa6cfd017618c661c7c993fc4872482a0e
Dan <pintea.dan988@gmail.com>
2022-11-30 19:32:15 +0100

    Fixed indentation.

--- constants/index.js
+++ constants/index.js
@@ -1,88 +1,88 @@
 export const exploreWorlds = [
-  {
-    id: 'world-1',
-    imgUrl: '/planet-01.png',
-    title: 'The Known World',
-  },
-  {
-    id: 'world-2',
-    imgUrl: '/planet-02.png',
-    title: 'The Upside Down',
-  },
-  {
-    id: 'world-3',
-    imgUrl: '/planet-03.png',
-    title: 'Kadirojo Permai',
-  },
-  {
-    id: 'world-4',
-    imgUrl: '/planet-04.png',
-    title: 'Paradise Island',
-  },
-  {
-    id: 'world-5',
-    imgUrl: '/planet-05.png',
-    title: 'Hawkins Labs',
-  },
+    {
+        id: 'world-1',
+        imgUrl: '/planet-01.png',
+        title: 'The Known World',
+    },
+    {
+        id: 'world-2',
+        imgUrl: '/planet-02.png',
+        title: 'The Upside Down',
+    },
+    {
+        id: 'world-3',
+        imgUrl: '/planet-03.png',
+        title: 'Kadirojo Permai',
+    },
+    {
+        id: 'world-4',
+        imgUrl: '/planet-04.png',
+        title: 'Paradise Island',
+    },
+    {
+        id: 'world-5',
+        imgUrl: '/planet-05.png',
+        title: 'Hawkins Labs',
+    },
 ];
 
 export const startingFeatures = [
-  'Find a world that suits you and you want to enter',
-  'Enter the world by reading basmalah to be safe',
-  'No need to beat around the bush, just stay on the gas and have fun',
+    'Find a world that suits you and you want to enter',
+    'Enter the world by reading basmalah to be safe',
+    'No need to beat around the bush, just stay on the gas and have fun',
 ];
 
 export const newFeatures = [
-  {
-    imgUrl: '/vrpano.svg',
-    title: 'Beware,the new world is here',
-    subtitle:
-        'The metaverse will likely have sweeping consequences for human behavior.',
-  },
-  {
-    imgUrl: '/headset.svg',
-    title: 'More realistic than the real thing',
-    subtitle:
-        'The metaverse will create new opportunities for improving behavior and mental health.',
-  }
+    {
+        imgUrl: '/vrpano.svg',
+        title: 'Beware,the new world is here',
+        subtitle:
+            'The metaverse will likely have sweeping consequences for human behavior.',
+    },
+    {
+        imgUrl: '/headset.svg',
+        title: 'More realistic than the real thing',
+        subtitle:
+            'The metaverse will create new opportunities for improving behavior and mental health.',
+    }
 ];
 
 export const insights = [
-  {
-    imgUrl: '/planet-06.png',
-    title: 'Why metaverse real estate is selling for millions',
-    subtitle:
-        'Metaverse real estate values are skyrocketing, with one plot selling for a record €2.49mil. Decentraland\'s Sam Hamilton explains why.',
-  },
-  {
-    imgUrl: '/planet-07.png',
-    title: '7 tips to easily master the madness of the Metaverse',
-    subtitle:
-        'For creators, Metaverse saw more opportunities than simply playing games.',
-  },
-  {
-    imgUrl: '/planet-08.png',
-    title: 'How the fashion industry is getting a makeover from the Metaverse',
-    subtitle:
-        'The fashion industry is cashing in on the Metaverse, using the platform to reach new markets and promote its wares.',
-  },
+    {
+        imgUrl: '/planet-06.png',
+        title: 'Why metaverse real estate is selling for millions',
+        subtitle:
+            'Metaverse real estate values are skyrocketing, with one plot selling for a record €2.49mil. Decentraland\'s Sam Hamilton explains why.',
+    },
+    {
+        imgUrl: '/planet-07.png',
+        title: '7 tips to easily master the madness of the Metaverse',
+        subtitle:
+            'For creators, Metaverse saw more opportunities than simply playing games.',
+    },
+    {
+        imgUrl: '/planet-08.png',
+        title: 'How the fashion industry is getting a makeover from the Metaverse',
+        subtitle:
+            'The fashion industry is cashing in on the Metaverse, using the platform to reach new markets and promote its wares.',
+    },
 ];
 
 export const socials = [
-  {
-    name: 'twitter',
-    url: '/twitter.svg',
-  },
-  {
-    name: 'linkedin',
-    url: '/linkedin.svg',
-  },
-  {
-    name: 'instagram',
-    url: '/instagram.svg',
-  },
-  {
-    name: 'facebook',
-    url: '/facebook.svg',
-  },
+    {
+        name: 'twitter',
+        url: '/twitter.svg',
+    },
+    {
+        name: 'linkedin',
+        url: '/linkedin.svg',
+    },
+    {
+        name: 'instagram',
+        url: '/instagram.svg',
+    },
+    {
+        name: 'facebook',
+        url: '/facebook.svg',
+    },
 ];
